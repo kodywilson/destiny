0f2bcd319043815001fedf12582bbd12dff8d424
kodywilson <kodywilson@gmail.com>
2014-03-04 07:48:33 -0800

    Fixed destiny.rb

--- lib/destiny.rb
+++ lib/destiny.rb
@@ -3,15 +3,16 @@ class NewGame
   
   def initialize new_game
     @new_game = new_game
+    puts @new_game
   end
   
   def game_select
     #should probably be a case statement that verifies that only yes, y, no, or n was entered
     # and ignores caps (lower and upper are valid)
     if @new_game == "yes" || "y"
-      puts "Starting a new game, please enter character name."
+      "Starting a new game, please enter character name."
     else
-      puts "Please select a game to load."
+      "Please select a game to load."
     end
   end
   
